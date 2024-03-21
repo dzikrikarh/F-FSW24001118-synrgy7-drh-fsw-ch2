d606baa7abb560c2e54c9549837e64e2aa0d9ac8
Dzikrika Rahmatu Hayati <dzikrikahayati@gmail.com>
2024-03-21 22:46:55 +0700

    bismillah pertama

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    stagePadding: 70,
+    autoWidth: true,
+    items: 3,
+    center: true,
+    responsiveClass: true,
+    nav: true,
+    responsive: false,
+  });
+  
